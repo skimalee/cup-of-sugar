bbb0f197b32d5dd19ba839bc7647cd32ce3c9b92
Daniel Beck <dlc.beck@gmail.com>
2020-03-23 11:39:38 -0700

    rafactor models

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Profile
 
 CATEGORIES = (
     ('A', 'Food'),
@@ -20,20 +20,27 @@ CUP_TYPE = (
 
 # Create your models here.
 
+
 class Chat(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    messager = models.ForeignKey(User, on_delete=models.CASCADE)
+    user1_id = models.IntegerField()
+    user1_name = models.CharField(max_length=50)
+    user2_id = models.IntegerField()
+    user2_name = models.CharField(max_length=50)
 
     def __str__(self):
         return f"Chat between {self.owner}, {self.messager}"
 
+
 class Message(models.Model):
     content = models.CharField(max_length=160)
-    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    sender_name = models.CharField(max_length=50)
+    sender_id = models.IntegerField()
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
 
+
 class Cup(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    fulfilled_by_profile_id = models.IntegerField()
     cup_type = models.CharField(
         max_length=1,
         choices=CUP_TYPE
@@ -49,12 +56,12 @@ class Cup(models.Model):
         return f"{self.cup_type} cup {self.item} in {self.category} is {'fulfilled' if self.fulfilled else 'not fulfilled'}"
 
 
-class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    display_name = models.CharField(max_length=50) 
-    zipcode = models.IntegerField(max_length=5) 
-    email = models.EmailField()
+class User_Profile(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=50)
+    zip = models.IntegerField()
+    chats = models.ManyToManyField(Chat)
+    cups_filled = models.IntegerField()
 
     def __str__(self):
         return self.user
-
