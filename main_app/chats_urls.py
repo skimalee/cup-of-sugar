d47223c6a981f9f9c4979246ced062e3afc0c79e
Daniel Beck <dlc.beck@gmail.com>
2020-03-26 15:55:51 -0700

    add create and read messages

--- main_app/chats_urls.py
+++ main_app/chats_urls.py
@@ -3,5 +3,7 @@ from . import chats_views
 
 urlpatterns = [
     path('', chats_views.chats_index, name='chats_index'),
-    path('<int:chat_id>', chats_views.chats_detail, name='chats_detail'),
+    path('<int:chat_id>/', chats_views.chats_detail, name='chats_detail'),
+    path('create/', chats_views.chats_create, name='chats_create'),
+    path('<int:chat_id>/add_message/', chats_views.add_message, name='add_message'),
 ]
